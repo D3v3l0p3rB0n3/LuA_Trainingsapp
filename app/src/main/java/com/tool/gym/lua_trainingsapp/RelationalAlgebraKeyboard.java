ad5dcc7e313b78c04f0d170c3c2f85927310d8d3
D3v3l0p3rB0n3 <Felix.Grabscheit@gmail.com>
2017-05-12 00:14:10 +0200

    Tastatur erstellt

---
+++ app/src/main/java/com/tool/gym/lua_trainingsapp/RelationalAlgebraKeyboard.java
@@ -0,0 +1,91 @@
+package com.tool.gym.lua_trainingsapp;
+
+import android.app.Service;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * Created by matthiasbrandel on 11.05.17.
+ */
+
+public class RelationalAlgebraKeyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+    private boolean caps = false;
+
+    public View onCreateInputView() {
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this,R.xml.keyboard_relational);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+
+
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+        InputConnection ic = getCurrentInputConnection();
+        switch (i) {
+            case Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1,0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) i;
+                if(Character.isLetter(code) && caps)
+                {
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
